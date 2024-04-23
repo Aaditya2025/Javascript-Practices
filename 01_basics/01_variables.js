40e005eb831a753710b9923806b350316765f820
Aditya Yadav <103239471+Aditya00003@users.noreply.github.com>
2024-04-23 20:22:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+var accountID = 13242; 
+let name = "adsit "; 
+const nameID = 2342; 
+console.log(name); 
+console.log(nameID); 
+console.log(accountID); 
+
+
+// 1. Difference between "var, const and let":
+// ⭕ "var" old js (es5) me tha. AND "let, const" new js (es6) me hai.
+// ⭕ "var" function scoped hota hai.: var keyword se bana varialbe apne parent function me kahi bhi use ho sakta hai ye baaki languages me aesa nhi hota.
+// "let, const" braces scoped hota hai.: ye dono keyword se bana hua variable sabhi languages ke varialbes ki tarah kaam karta hai. 
+// ⭕ "var" add itself to the window object. 
+// "let, const" doesn't adds itself to the window object.  
+ 
