4aa5eca8bb1b8b66426aa49a3a6f8268bc71093a
Aditya Yadav <103239471+Aditya00003@users.noreply.github.com>
2024-04-25 19:20:14 +0000

    array completed

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// It follow the properties of shallow copy and in js arryas are resizable. 
+// Shallow copy of an object is a copy whose properties share the same references.
+// We put any datatype of values in an aarray. 
+
+const arr = [0, 1, 2, 3, 4, 5] ; 
+// console.log(arr[1]);
+
+//Method: 2
+
+// arr.push(5); 
+// arr.push(234); 
+// arr.pop(); 
+
+// arr.unshift(3); 
+// arr.shift();
+
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(6));
+
+// const newarr = arr.join();  //It will convert the arr into string.
+
+// console.log(arr);
+// console.log(typeof newarr);
+
+
+// SLICE AND SPLICE : Slice will not change the original array but splice will change the original array as we see in the below example.
+
+console.log("A",  arr);
+const myn1 = arr.slice(1,3)
+
+console.log(myn1); 
+console.log("B", arr); 
+
+const myn2 = arr.splice(1,3)
+console.log("C", arr)
+console.log(myn2); 
---
+++ 02_basics/02_array.js
@@ -0,0 +1,38 @@
+// Array apne indices ke andar kisi bhi type ke data ko insert kar leta hai jaise abhi ham dekhne wale hain ki kaise ek aaray apne laast indices pe pura ek array ko rakh leta hai. 
+
+const marvel_heros = ["ironman" , "thor", "spiderman"]; 
+const dc_heros = ["superman", "flash", "batman"]; 
+
+// marvel_heros.push(dc_heros); 
+// console.log(marvel_heros);  //In the result we see that in "marvel_heros" array's 3 index have the dc_heros arrays.
+// console.log(marvel_heros[3][1])
+
+
+
+// const allHeros = marvel_heros.concat(dc_heros);   //It is better than push but mostly developer use spread operator.
+// console.log(allHeros);
+
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros];  
+// console.log(all_new_heros);              //This (...) is the spread operator which means all values is spread.
+
+
+
+// const anotherArray = [1, 2, 3, [4, 5, 6], [2, 8, [1, 9]],3]
+// const realanotherArray = anotherArray.flat(Infinity)   //"flat": It will break all the element into a single array.
+// console.log(realanotherArray);
+
+
+
+// console.log(Array.isArray("Aditya"));
+// console.log(Array.from("Aditya"));   //To create any value into an array.
+// console.log(Array.from({name: "Aditya"}));  //We have to tell that of which part you want to create array.
+
+
+
+const score1 = 100; 
+const score2 = 200; 
+const score3 = 300; 
+
+console.log(Array.of(score1, score2, score3));  //"Array.of" : It will also create a new array by using some values.
